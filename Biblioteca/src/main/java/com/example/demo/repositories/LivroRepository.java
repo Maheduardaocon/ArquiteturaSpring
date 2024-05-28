0a7c062ca612236eefaee91dd23430586f3f4993
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2024-05-28 11:51:02 -0300

    Atualizar o LivroRepository.java

--- Biblioteca/src/main/java/com/example/demo/repositories/LivroRepository.java
+++ Biblioteca/src/main/java/com/example/demo/repositories/LivroRepository.java
@@ -1,16 +1,9 @@
 package com.example.demo.repositories;
 
+import com.example.biblioteca.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import com.example.demo.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-	
-	@Query(value = "SELECT * FROM Livro l WHERE lower(l.titulo) LIKE %:titulo", nativeQuery = true)
-	List<Livro> buscarPorTitulo(@Param("titulo")String titulo);
-	
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+    List<Livro> findByAutorNome(String nome);
 }
