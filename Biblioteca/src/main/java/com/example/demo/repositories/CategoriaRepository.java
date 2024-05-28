8405ce0fbc055f5b0c92187d93e7192adc76f2d8
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2024-05-28 11:52:51 -0300

    Atualizar o CategoriaRepository.java

--- Biblioteca/src/main/java/com/example/demo/repositories/CategoriaRepository.java
+++ Biblioteca/src/main/java/com/example/demo/repositories/CategoriaRepository.java
@@ -1,16 +1,6 @@
 package com.example.demo.repositories;
 
-import java.util.List;
-
+import com.example.biblioteca.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.example.demo.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-	
-	@Query(value = "SELECT * FROM Categoria l WHERE lower(l.nome) LIKE %:nome", nativeQuery = true)
-	List<Categoria> buscarPorNome(@Param("nome")String nome);
-	
-}
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
