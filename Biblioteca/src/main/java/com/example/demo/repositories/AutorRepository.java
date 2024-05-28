4e2b691ec7f2762aa2e9aea4e67bb8c6c9424eb7
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2024-05-28 11:51:40 -0300

    Atualizar o AutorRepository.java

--- Biblioteca/src/main/java/com/example/demo/repositories/AutorRepository.java
+++ Biblioteca/src/main/java/com/example/demo/repositories/AutorRepository.java
@@ -1,17 +1,6 @@
 package com.example.demo.repositories;
 
-import java.util.List;
-
+import com.example.biblioteca.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.example.demo.entities.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Long>{
-	
-	@Query(value = "SELECT * FROM Autor l WHERE lower(l.nome) LIKE %:nome", nativeQuery = true)
-	List<Autor> buscarPorNome(@Param("nome")String nome);
-	
-	
-}
+public interface AutorRepository extends JpaRepository<Autor, Integer> {}
